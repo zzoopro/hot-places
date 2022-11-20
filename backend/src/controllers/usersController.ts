b5246227bd3f6b05f839dfcee13a18e1ec84f563
zzoo <zzoolink@gmail.com>
2022-11-21 03:18:14 +0900

    auth ing...

--- backend/src/controllers/usersController.ts
+++ backend/src/controllers/usersController.ts
@@ -1,6 +1,8 @@
 import { Express, Request, Response } from "express";
 import { User } from "../db/schema";
 import crypto from "crypto";
+import jwt from "jsonwebtoken";
+
 import { isEmpty } from "../utils";
 
 export const signup = async (req: Request, res: Response) => {
@@ -22,9 +24,17 @@ export const signup = async (req: Request, res: Response) => {
         .digest("base64");
       const newUser = new User({ email, password: hashed, phone, username });
       if (newUser) {
+        let token;
+        try {
+          token = jwt.sign({ userId: newUser._id }, process.env.PRIVATE_KEY, {
+            expiresIn: "1h",
+          });
+        } catch (error) {
+          res.status(500).json({ ok: false, message: "token create failed." });
+        }
         await newUser.save();
         console.log("sign up completed");
-        return res.json({ ok: true });
+        return res.json({ ok: true, token });
       }
     }
   }
@@ -44,7 +54,16 @@ export const login = async (req: Request, res: Response) => {
     ) {
       return res.status(400).json({ ok: false, message: "password incorrect" });
     }
+
+    let token;
+    try {
+      token = jwt.sign({ userId: user._id }, process.env.PRIVATE_KEY, {
+        expiresIn: "1h",
+      });
+    } catch (error) {
+      res.status(500).json({ ok: false, message: "token create failed." });
+    }
     console.log("logined");
-    return res.json({ ok: true });
+    return res.json({ ok: true, token });
   }
 };
